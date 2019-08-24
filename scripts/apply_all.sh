7a08392a0ad6da6cd29d0c2c2588ddec52422b30
José Vidal <javidgon@gmail.com>
2019-08-24 21:14:16 +0200

    Fix up some issues with the Helm Chart

--- scripts/apply_all.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl apply -f ../prod/battleforcastile_match_recorder_secrets.yml
-kubectl create secret generic cloudsql-instance-credentials --from-file=credentials.json=../prod/key.json
-
-kubectl apply -f ../deployment.yml
-kubectl apply -f ../service.yml
-kubectl apply -f ../ingress.yml
--- scripts/update_secret.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl apply -f ../prod/battleforcastile_match_recorder_secrets.yml
-kubectl create secret generic cloudsql-instance-credentials --from-file=credentials.json=../prod/key.json
-
-kubectl scale deployment battleforcastile-match-recorder-deployment --replicas=0
-sleep 15;
-kubectl scale deployment battleforcastile-match-recorder-deployment --replicas=1
